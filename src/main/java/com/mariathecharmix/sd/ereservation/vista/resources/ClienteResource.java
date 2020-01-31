3cc3cc98960cb1175fe10ae0a4e3765e5ccd852f
MariaPaulaS <mariathecharmix@hotmail.com>
2020-01-31 11:24:44 -0500

    Implementacion base de servicio web Cliente

---
+++ src/main/java/com/mariathecharmix/sd/ereservation/vista/resources/ClienteResource.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mariathecharmix.sd.ereservation.vista.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Clase que representa el servicio web de cliente, que expone las operaciones de la
+ * clase service
+ * @author maria
+ *
+ */
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteResource {
+
+}
